50c548b82c3e65e298f0e4ff0059ef3e0341e6b5
Seiry <yuwenjie66@126.com>
2017-05-09 10:24:33 +0800

    合并api，解决冲突

--- api/index.php
+++ api/index.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
 
@@ -10,8 +9,6 @@ ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
 */
 
-=======
->>>>>>> f175c474ab35756dcdbc2a719efc4b7047db639c
 require_once '../include/config.php';
 require_once '../include/db.class.php';
 require_once '../include/request.class.php';
@@ -25,11 +22,9 @@ DB::connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_PORT);
 Request::initialize();
 
 // white list
-<<<<<<< HEAD
+
 $actionList = ['getBottle', 'getComments', 'getLike', 'getMoreBottles', 'getMyBottles', 'getUser', 'postBottle', 'postComment',"getTopic"];
-=======
-$actionList = ['getBottle', 'getComments', 'getLike', 'getMoreBottles', 'getMyBottles', 'getUser', 'postBottle', 'postComment'];
->>>>>>> f175c474ab35756dcdbc2a719efc4b7047db639c
+
 
 if (!isset($_GET['_action'])) {
     Result::error('missing _action');
