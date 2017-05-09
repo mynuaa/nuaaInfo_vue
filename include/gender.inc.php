f34430ad396b7acba673994a59851eb35fc951af
Seiry <yuwenjie66@126.com>
2017-05-09 11:23:43 +0800

    后端产生头像

--- include/gender.inc.php
+++ include/gender.inc.php
@@ -3,8 +3,10 @@
 function genderProcess (&$value){
     if($value['gender'] == 1){
         $value['nickname'] = '某男生';
+        $value['avatar'] = '/bottle/avatar/boy' . ($value['id'] % 6 + 1 ) . '.jpg';
     }else{
         $value['nickname'] = '某女生';
+        $value['avatar'] = '/bottle/avatar/girl' . ($value['id'] % 6 + 1 ) . '.jpg';
     }
     $value['userId'] = '0';
 }
